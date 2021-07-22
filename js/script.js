d50af11cc4cc0ac93aaf30f0a6de87cdb96b3f14
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-07-22 10:39:51 +0200

    add list items by pressing enter key (ascii code 13) and writing cleaner code

--- js/script.js
+++ js/script.js
@@ -1,50 +1,43 @@
+function newItem() {
 
-function newItem(){
-  
-//1. Adding a new item to the list of items: 
-let li = $('<li></li>');
-let inputValue = $('#input').val(); //.val() delivers the value of the input
-li.append(inputValue);
+	//1. Adding a new item to the list of items: 
+	let listItem = $('<li></li>');
+	let inputValue = $('#input').val(); //.val() delivers the value of the input
+	listItem.append(inputValue);
 
-if (inputValue === '') {
-  alert('You must write something!');
-} else {
-  $('#list').append(li);
+	if (inputValue === '') {
+		alert('You must write something!');
+	} else {
+		$('#list').append(listItem);
+	}
+
+	//2. Crossing out an item from the list of items:
+	function crossOut() {
+		listItem.toggleClass('strike');
+	}
+
+	listItem.on('dblclick', crossOut);
+
+	//3(i). Adding the delete button "X": 
+	let crossOutButton = $('<button class="crossOutButton"></button>');
+	crossOutButton.append("X");
+	listItem.append(crossOutButton);
+
+	crossOutButton.on('click', deleteListItem);
+	//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+	function deleteListItem() {
+		listItem.addClass('delete')
+	}
+	// 4. Reordering the items: 
+	$('#list').sortable();
 }
 
-//2. Crossing out an item from the list of items:
-function crossOut() {
-  li.toggleClass('strike');
-}
-
-li.on('dblclick', crossOut);
-
-//3(i). Adding the delete button "X": 
-let crossOutButton = $('<button class="crossOutButton"></button>');
-crossOutButton.append(crossOutButton.text("X"));
-li.append(crossOutButton);
-
-crossOutButton.on('click', deleteListItem);
-//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-function deleteListItem(){
-  li.addClass('delete')
-}
-// 4. Reordering the items: 
-$('#list').sortable();
-
-$('#input').on('keypress', function(event){
-  if(event.which === 13){
-    $('li').each(function() {
-      event.preventDefault();
-      $(this).append(inputValue);
-      $('#list').append(li);
-    })
-  }
-    /*.append($('#input').val());
-    $('#list').append(li);
-    event.preventDefault();*/
-  
-});
-
-}
-
+	// 5. Adding List-Items by pressing Enter-key
+  let inputValue = $('#input').val();
+  $('#input').on('keydown', function(e) {
+    if (e.which == '13') {
+      newItem();
+      e.preventDefault();
+      $('#input').val('');
+    }
+  });
